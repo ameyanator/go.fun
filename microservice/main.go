a10f384cadfdd057f414fbd698f2a3de2285ec32
Ameya Sinha <ameyaosd@gmail.com>
2022-10-11 05:49:14 +0000

    Setup WebServer and Make requests to it

---
+++ microservice/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// Handle func registers a func to a path on DefaultServMux
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World!")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+		// log.Printf("Data %s\n", d)
+		fmt.Fprintf(rw, "Hello %s\n", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":9090", nil) // the second parameter is root handler and by default its DefaultSerMux
+}
