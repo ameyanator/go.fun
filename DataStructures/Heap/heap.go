d2705523f053dd8e040a791e5737abed9dc7d2ce
Ameya Sinha <ameyaosd@gmail.com>
2023-12-26 10:35:41 +0000

    generic heap

--- DataStructures/Heap/heap.go
+++ DataStructures/Heap/heap.go
@@ -7,52 +7,79 @@ import (
 
 // Item represents an item in the heap.
 type Item[T any] struct {
-	Value    T
-	Priority int
+	Value T
 }
 
-// PriorityQueue implements a min-heap based on the Priority field of Item.
-type PriorityQueue[T any] []*Item[T]
+// PriorityQueue implements a heap based on the comparison function cmp.
+type PriorityQueue[T any] struct {
+	Items []Item[T]
+	cmp   func(a, b T) bool
+}
+
+// NewPriorityQueue creates a new PriorityQueue with the specified comparison function.
+func NewPriorityQueue[T any](cmp func(a, b T) bool) *PriorityQueue[T] {
+	return &PriorityQueue[T]{cmp: cmp}
+}
 
 // Len returns the number of elements in the heap.
-func (pq PriorityQueue[T]) Len() int { return len(pq) }
+func (pq *PriorityQueue[T]) Len() int { return len(pq.Items) }
 
 // Less returns whether the element with index i should sort before the element with index j.
-func (pq PriorityQueue[T]) Less(i, j int) bool {
-	return pq[i].Priority < pq[j].Priority
+func (pq *PriorityQueue[T]) Less(i, j int) bool {
+	return pq.cmp(pq.Items[i].Value, pq.Items[j].Value)
 }
 
 // Swap swaps the elements with indexes i and j.
-func (pq PriorityQueue[T]) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
+func (pq *PriorityQueue[T]) Swap(i, j int) {
+	pq.Items[i], pq.Items[j] = pq.Items[j], pq.Items[i]
 }
 
 // Push adds an element to the heap.
 func (pq *PriorityQueue[T]) Push(x interface{}) {
-	item := x.(*Item[T])
-	*pq = append(*pq, item)
+	item := x.(Item[T])
+	pq.Items = append(pq.Items, item)
 }
 
-// Pop removes and returns the minimum element from the heap.
+// Pop removes and returns the element from the heap.
 func (pq *PriorityQueue[T]) Pop() interface{} {
-	old := *pq
+	old := pq.Items
 	n := len(old)
 	item := old[n-1]
-	*pq = old[0 : n-1]
+	pq.Items = old[0 : n-1]
 	return item
 }
 
 func main() {
-	pq := make(PriorityQueue[string], 0)
+	// Create a min-heap
+	minHeap := NewPriorityQueue[int](func(a, b int) bool {
+		return a < b
+	})
 
-	// Push items onto the heap.
-	heap.Push(&pq, &Item[string]{Value: "foo", Priority: 3})
-	heap.Push(&pq, &Item[string]{Value: "bar", Priority: 1})
-	heap.Push(&pq, &Item[string]{Value: "baz", Priority: 2})
+	// Push items onto the min heap.
+	heap.Push(minHeap, Item[int]{Value: 3})
+	heap.Push(minHeap, Item[int]{Value: 1})
+	heap.Push(minHeap, Item[int]{Value: 2})
 
-	// Pop items from the heap in order of priority.
-	for pq.Len() > 0 {
-		item := heap.Pop(&pq).(*Item[string])
-		fmt.Printf("%s:%d ", item.Value, item.Priority)
+	// Pop items from the min heap.
+	for minHeap.Len() > 0 {
+		item := heap.Pop(minHeap).(Item[int])
+		fmt.Printf("%d ", item.Value)
+	}
+	fmt.Println()
+
+	// Create a max-heap
+	maxHeap := NewPriorityQueue[int](func(a, b int) bool {
+		return a > b
+	})
+
+	// Push items onto the max heap.
+	heap.Push(maxHeap, Item[int]{Value: 3})
+	heap.Push(maxHeap, Item[int]{Value: 1})
+	heap.Push(maxHeap, Item[int]{Value: 2})
+
+	// Pop items from the max heap.
+	for maxHeap.Len() > 0 {
+		item := heap.Pop(maxHeap).(Item[int])
+		fmt.Printf("%d ", item.Value)
 	}
 }
